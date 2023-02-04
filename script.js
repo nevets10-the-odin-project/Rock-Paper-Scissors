813980e53e30b84badbe9b7523e25929fe2cfb65
Nevets <12600557+Nevets10@users.noreply.github.com>
2023-02-03 19:38:11 -0800

    Add result conditions to playRound

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const playerSelection = "Rock";
+const playerSelection = "Scissors";
 const computerSelection = getComputerChoice();
 
 playRound(playerSelection, computerSelection);
@@ -13,5 +13,18 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
-	console.log(computerSelection);
+	console.log("Player", playerSelection);
+	console.log("Computer", computerSelection);
+
+	if (playerSelection === computerSelection) {
+		console.log("Tie!");
+	} else if (playerSelection === "Rock" && computerSelection === "Scissors") {
+		console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
+	} else if (playerSelection === "Paper" && computerSelection === "Rock") {
+		console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
+	} else if (playerSelection === "Scissors" && computerSelection === "Paper") {
+		console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
+	} else {
+		console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
+	}
 }
