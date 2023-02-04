b16fa15b50b0ac798360bee46c412fe9da507eab
Nevets <12600557+Nevets10@users.noreply.github.com>
2023-02-03 19:46:17 -0800

    Add getPlayerChoice

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 "use strict";
 
-const playerSelection = "Scissors";
+const playerSelection = getPlayerChoice();
 const computerSelection = getComputerChoice();
 
 playRound(playerSelection, computerSelection);
+
+function getPlayerChoice() {
+	const choice = prompt("Rock, Paper or Scissors?", "");
+	const lowerCaseChoice = choice.toLocaleLowerCase();
+	const capitalizedChoice =
+		lowerCaseChoice.charAt(0).toLocaleUpperCase() + lowerCaseChoice.slice(1);
+
+	return capitalizedChoice;
+}
 
 function getComputerChoice() {
 	const choices = ["Rock", "Paper", "Scissors"];
