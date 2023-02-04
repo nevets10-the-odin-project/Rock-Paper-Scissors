569e6311b4b885cf866fc22857a139a1145152fd
Nevets <12600557+Nevets10@users.noreply.github.com>
2023-02-03 19:13:25 -0800

    Add getComputerChoice function

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 "use strict";
 
-console.log("Hello there");
+function getComputerChoice() {
+    const choices = ["Rock", "Paper", "Scissors"];
+    const randomNumber = Math.floor(Math.random() * choices.length);
+
+    console.log(randomNumber);
+    console.log(choices[randomNumber]);
+}
+
+getComputerChoice();
